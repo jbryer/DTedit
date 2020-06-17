a769e75ce847044f132e1df367904ca3a71dd4da
David Fong <vkelim@bigpond.com>
2020-06-17 10:35:33 +1000

    add Sys.sleep to fileinput_modular test

--- tests/fileinput_modular/tests/test.R
+++ tests/fileinput_modular/tests/test.R
@@ -9,19 +9,24 @@ app$uploadFile(`Grocery_List-editdt_edit_Picture` = "bee_agapanthus.jpg")
 # <-- This should be the path to the file, relative to the app's tests/ directory
 app$uploadFile(`Grocery_List-editdt_edit_Spreadsheet` = "DiabetesSIP_Report.csv")
 app$setInputs(`Grocery_List-editdt_update` = "click")
+Sys.sleep(1.5)
+
 app$setInputs(`Grocery_List-editdt_add` = "click")
 app$setInputs(`Grocery_List-editdt_add_Buy` = "Locquats",
               `Grocery_List-editdt_add_Quantity` = 3)
 app$uploadFile(`Grocery_List-editdt_add_Picture` = "epiphyllum.jpg")
 app$uploadFile(`Grocery_List-editdt_add_Spreadsheet` = "Zostavax_Report.csv")
 app$setInputs(`Grocery_List-editdt_insert` = "click")
+Sys.sleep(1.5)
 
 app$executeScript('document.getElementById("picture_1").click()')
 app$executeScript('document.getElementById("spreadsheet_1").click()')
+Sys.sleep(1.5)
 app$snapshot(items = list(export = "spreadsheet_list"))
 
 app$executeScript('document.getElementById("picture_4").click()')
 app$executeScript('document.getElementById("spreadsheet_4").click()')
+Sys.sleep(1.5)
 app$snapshot(items = list(export = "spreadsheet_list"))
 # on the third snapshot, just check spreadsheet_list
 # because thedata is a large JSON (with two JPGs, total 1.2 MB)
