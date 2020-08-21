b71bc826c229b77af28ca6be10a15edcd6e4682b
David Fong <vkelim@bigpond.com>
2020-08-21 22:05:03 +1000

    example_mod.R and fileInput_module modularized

--- inst/examples/example_mod.R
+++ inst/examples/example_mod.R
@@ -4,14 +4,20 @@
 # unfortunately, this application cannot be
 # tried with 'example("dteditmod")', but you can copy
 # and paste to execute in 'interactive' console mode,
-# or copy the lines into an '.R' file and choose 
+# or copy the lines into an '.R' file and choose
 # 'Run App' from RStudio.
 library(shiny)
 library(DTedit)
 
-server <- function(input, output, session) {
-  
-  Grocery_List <- callModule(
+myModuleUI <- function(id) {
+  ns <- shiny::NS(id)
+  shiny::tagList(
+    dteditmodUI(ns('Grocery_List'))
+  )
+}
+
+myModule <- function(input, output, session) {
+  Grocery_List_Results <- shiny::callModule(
     dteditmod,
     id = 'Grocery_List',
     thedata = data.frame(
@@ -22,9 +28,15 @@ server <- function(input, output, session) {
   )
 }
 
+server <- function(input, output, session) {
+
+  shiny::callModule(myModule, 'myModule1')
+
+}
+
 ui <- fluidPage(
   h3('Grocery List'),
-  dteditmodUI('Grocery_List')
+  myModuleUI('myModule1')
 )
 
 if (interactive() || isTRUE(getOption("shiny.testmode")))
