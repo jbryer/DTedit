1afba508b0082b90527b82e854975d542ed70b94
David Fong <vkelim@bigpond.com>
2020-08-22 13:28:10 +1000

    dtedit_demo_modular test

--- tests/dtedit_demo_modular/tests/test.R
+++ tests/dtedit_demo_modular/tests/test.R
@@ -2,19 +2,19 @@ app <- ShinyDriver$new("../")
 app$snapshotInit("test")
 
 app$setInputs(
-  `books-editdtdt_rows_selected` = 6, allowInputNoBinding_ = TRUE
+  `myBooks-books-editdtdt_rows_selected` = 6, allowInputNoBinding_ = TRUE
 )
 app$setInputs(
-  `books-editdtdt_rows_last_clicked` = 6, allowInputNoBinding_ = TRUE
+  `myBooks-books-editdtdt_rows_last_clicked` = 6, allowInputNoBinding_ = TRUE
 )
-app$setInputs(`books-editdt_copy` = "click")
+app$setInputs(`myBooks-books-editdt_copy` = "click")
 app$setInputs(
-  `books-editdt_add_Authors` = c("Chambers, J.M.", "Hastie, T.J.", "Becker, R.A.", "Wilks, A.R."),
-  `books-editdt_add_Date` = "1991-01-17",
-  `books-editdt_add_Publisher` = "Wiley",
-  `books-editdt_add_Title` = "Statistical Models in S - 2nd edition"
+  `myBooks-books-editdt_add_Authors` = c("Chambers, J.M.", "Hastie, T.J.", "Becker, R.A.", "Wilks, A.R."),
+  `myBooks-books-editdt_add_Date` = "1991-01-17",
+  `myBooks-books-editdt_add_Publisher` = "Wiley",
+  `myBooks-books-editdt_add_Title` = "Statistical Models in S - 2nd edition"
 )
-app$setInputs(`books-editdt_insert` = "click")
+app$setInputs(`myBooks-books-editdt_insert` = "click")
 
 Sys.sleep(2)
 app$snapshot(items = list(export = TRUE))
